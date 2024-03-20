ff32f98a3b700f29470a2017df3aaa833d1b90f5
a1113349 <161301277+a1113349@users.noreply.github.com>
2024-03-20 15:30:01 +0800

    Add files via upload
    Signed-off-by: a1113349 <161301277+a1113349@users.noreply.github.com>

---
+++ week03/introduce.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<meta charset"utf-8">
+<meta name="孫晛茗">
+<title>孫晛茗的自我介紹
+</title>
+</head>
+<body bgcolor="skyblue">
+<font color="black">
+<center><h2>孫晛茗的自我介紹</h2></center>
+<hr width="50%"color="gray">
+<a name="top">
+<center>
+<a href="me.png">
+<img src="me.png"wdith="200"height="200"border="1">
+</a>
+</center>
+<h3>介紹:</h3><br/>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp我叫<b>孫晛茗</b>，出生於台南市，現在就讀國立高雄大學資訊管理學系。我的父親從商，我的母親則身為全職家庭主婦，還有兩位兄弟姐妹，其中一位為比我年長兩歲的姊姊，另一位則為與我出生相差一分鐘的雙胞胎妹妹，致使我學習道路上並非孤軍奮戰。我有一個和諧快樂的家庭，家庭的管教方式為『開明』，從小爸爸教導我們要勇敢並且正向的面對問題，因此造就了我開朗的個性。
+<br/>
+<h3>基本資料:</h3><br/>
+<center>
+<table border="1"bgcolor="white">
+<tr><td>姓名</td><td>孫晛茗</td></tr>
+<tr><td>暱稱</td><td>麵線</td></tr>
+<tr><td>個人興趣</td><td>外向<br/>開朗</td></tr>
+<tr><td>連絡電話</td><td>0972119522</td></tr>
+<tr><td>血型</td><td>o</td></tr>
+<tr><td>星座</td><td>摩羯座</td></tr>
+<tr><td>生日</td><td>2004/01/09</td></tr>
+<tr><td>性別</td><td>女</td></tr>
+<tr><td>個人興趣</td><td>游泳<br/>旅遊<br/>繪畫</td></tr>
+<tr><td>特殊經歷</td><td>救生員執照合格<br/>中學生運動會游泳-接力第五名
+<br/>全國中學生音樂比賽打擊樂團體組總決賽優等</td></tr>
+</table>
+
+
+
+
+
+到底囉，<a href="#top">回開頭</a><br/>
+</center>
+
+</body>
+</html>
